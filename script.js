a87cf847152be882c5089a1cde1179b9e11274dc
keiner <keinergodinez@gmail.com>
2022-10-24 19:50:43 -0600

    inputs

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
     $('.child').on('click', function () {
-        //eliminar todos los elementos con la clase cuando esta activo.child
-
         $('.child').removeClass('active');
-
-        //agregar la clase active al elemento que se le dio click
         $(this).addClass('active');
-
     });
 });
