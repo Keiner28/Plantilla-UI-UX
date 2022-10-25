91209c559e9a0067fecfc1df4abea1ce68ac8df5
keiner <keinergodinez@gmail.com>
2022-10-25 15:49:19 -0600

    Ajustes

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
         $('.menu-item').removeClass('menu-item-active');
         $(this).addClass('menu-item-active');
     });
+
+    $('.menu-toggle').on('click', function () {
+        $('.menu-toggle').toggleClass('is-active');
+        $('.sidebar').toggleClass('is-active');
+    });
 });
